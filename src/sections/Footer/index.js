ac9e4e6b4c489e008893fd0252df326f613ab271
Urs Schmidt <urs.schmidt@ts.berlin>
2022-05-16 13:31:42 +0200

    footer add extra top wrapper

--- src/sections/Footer/index.js
+++ src/sections/Footer/index.js
@@ -5,41 +5,43 @@ function Footer({ content, lang }) {
   return (
     <div className={cn.footer}>
       <div className={cn.top}>
-        <div className={cn.logos}>
-          {content.logos.map((d, i) => {
-            return (
-              <div
-                key={`logo-key-${i}`}
-                className={cx(cn.block, cn[d.className])}
-              >
-                <a
-                  className={cn.logoWrapper}
-                  href={d.href}
-                  target='_blank'
-                  rel='noreferrer'
-                >
-                  <img className={cn.logo} src={d.src[lang]} alt={d.alt} />
-                </a>
-              </div>
-            );
-          })}
-        </div>
-        <div className={cn.sitemap}>
-          <span className={cn.title}>Sitemap</span>
-          <div className={cn.sitemapLinks}>
-            {content.sitemap.links.map((d, i) => {
+        <div className={cn.topWrapper}>
+          <div className={cn.logos}>
+            {content.logos.map((d, i) => {
               return (
-                <a
-                  className={cn.sitemapLink}
-                  href={d.href}
-                  rel='noreferrer'
-                  target='_blank'
-                  key={i}
+                <div
+                  key={`logo-key-${i}`}
+                  className={cx(cn.block, cn[d.className])}
                 >
-                  {d.label}
-                </a>
+                  <a
+                    className={cn.logoWrapper}
+                    href={d.href}
+                    target='_blank'
+                    rel='noreferrer'
+                  >
+                    <img className={cn.logo} src={d.src[lang]} alt={d.alt} />
+                  </a>
+                </div>
               );
             })}
+          </div>
+          <div className={cn.sitemap}>
+            <span className={cn.title}>Sitemap</span>
+            <div className={cn.sitemapLinks}>
+              {content.sitemap.links.map((d, i) => {
+                return (
+                  <a
+                    className={cn.sitemapLink}
+                    href={d.href}
+                    rel='noreferrer'
+                    target='_blank'
+                    key={i}
+                  >
+                    {d.label}
+                  </a>
+                );
+              })}
+            </div>
           </div>
         </div>
       </div>
