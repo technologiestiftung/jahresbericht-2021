8dd3a71501519e0fcd1b06f1713fa5a50e5a2f28
Urs Schmidt <urs.schmidt@ts.berlin>
2022-05-31 17:49:37 +0200

    [TJ2-14] add click and hover to chart

--- src/components/PieChart/index.jsx
+++ src/components/PieChart/index.jsx
@@ -1,3 +1,4 @@
+import { useState } from "react";
 import { Chart as ChartJS, ArcElement, Tooltip } from "chart.js";
 import { Doughnut } from "react-chartjs-2";
 import cn from "./PieChart.module.scss";
@@ -6,31 +7,50 @@ import Legend from "./Legend";
 
 ChartJS.register(ArcElement, Tooltip);
 
-export const data = {
-  labels,
-  datasets: [
-    {
-      label: "Mittelherkunft Jahr 2021",
-      data: datapoints,
-      backgroundColor: colors,
-    },
-  ],
-};
-
-export const options = {
-  plugins: {
-    legend: {
-      display: false,
-    },
-  },
-  cutout: "65%",
-};
-
 const PieChart = () => {
+  const [activeArc, activeArcSet] = useState({});
+
+  const data = {
+    labels,
+    datasets: [
+      {
+        label: "Mittelherkunft Jahr 2021",
+        data: datapoints,
+        backgroundColor: colors,
+      },
+    ],
+  };
+
+  const onChartClick = (_, elements) => {
+    const index = elements[0].index;
+    activeArcSet({
+      label: labels[index],
+    });
+  };
+
+  const onChartHover = (event, chartElement) => {
+    event.native.target.style.cursor = chartElement[0] ? "pointer" : "default";
+  };
+
+  const options = {
+    plugins: {
+      legend: {
+        display: false,
+      },
+    },
+    events: ["click", "mousemove"],
+    onClick: (_, elements) => onChartClick(_, elements),
+    onHover: (e, el) => onChartHover(e, el),
+    cutout: "65%",
+  };
+
   return (
     <div className={cn.wrapper}>
       <h1 className={cn.title}>{data.datasets[0].label.toUpperCase()}</h1>
-      <Doughnut data={data} options={options} />
+      <div className={cn.doughnutWrapper}>
+        <Doughnut data={data} options={options} />
+        {activeArc && <div className={cn.infoBox}>{activeArc.label}</div>}
+      </div>
       <Legend />
     </div>
   );
