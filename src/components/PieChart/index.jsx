c412c167a1e5263b2891ec341a644ba4dea2e8ad
Urs Schmidt <urs.schmidt@ts.berlin>
2022-06-07 13:58:18 +0200

    TJ2-14 update ESLint

--- src/components/PieChart/index.jsx
+++ src/components/PieChart/index.jsx
@@ -59,7 +59,7 @@ const PieChart = () => {
 
   const onChartHover = (event, chartElement) => {
     if (chartElement[0]) {
-      const index = chartElement[0].index;
+      // const index = chartElement[0].index;
       event.native.target.style.cursor = "pointer";
       //   if (index != tooltipData.index) {
       //     tooltipDataSet({
